5f13b8648461931db9b61330cde038a64fdd09eb
Eric Fode <Eric@heroku.com>
2013-06-05 16:47:37 -0700

    MVP

---
+++ web.js
@@ -0,0 +1,21 @@
+var static = require('node-static');
+ 
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new(static.Server)('./app');
+var port = process.env.PORT || 5000;
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+  file.serve(request, response, function(err, result){
+    if(err && (err.status === 404)){
+            //serve index.html on 404 as well
+            file.serveFile('/index.html', 200, {}, request, response);
+    }
+  })
+    });
+}).listen(port);
+
