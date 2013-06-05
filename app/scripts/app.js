a0898b0477954ad93db9f6222489b095e3d1e7b0
Eric Fode <Eric@heroku.com>
2013-06-05 14:59:18 -0700

    working on getting node server going

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,6 +3,7 @@
 var neovacUIApp = angular.module('neovacUIApp', ['ngResource', 'nvServices']);
 
 neovacUIApp.config(function ($locationProvider, $routeProvider) {
+  $locationProvider.html5Mode(true).hashPrefix('!');
   $routeProvider
     .when('/', {
       templateUrl: 'views/main.html',
