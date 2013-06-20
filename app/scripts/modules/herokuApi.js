cdd08472621c9e0f40931da6c9a4a5fbdd4ab01d
Eric Fode <ericfode@Erics-MacBook-Pro.local>
2013-06-20 16:47:10 -0700

    all the messages are mine

--- app/scripts/modules/herokuApi.js
+++ app/scripts/modules/herokuApi.js
@@ -1,27 +1,24 @@
 'use strict';
 
 angular.module('herokuAPI', ['ngCookies']).
-  provider('herokuKey',function(){
+  provider('herokuKey',function($http){
     var config ={
       localStorageName: 'herokuApi'
     };
 
+    $http.get('/auth').success(function(data) {
+      var api = data.auth;
+      localStorage.token = api;
+    });
+
     this.extendConfig = function(configExtension){
       config = angular.extend(config, configExtension);
     };
-    this.$get = function($http){
+    this.$get = function(){
       return {
         get: function(){
-          var api = {};
-          $http.get('/auth').success(function(data) {
-            api = data.auth;
-            localStorage.token = api
-          });
-          if( api === null){
             return localStorage.token;
           }
-          return api;
-        }
       };
     };
   });
